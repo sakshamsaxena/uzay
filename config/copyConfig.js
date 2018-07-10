e19bcb57a5bf8f79f8543345a4ad0d40d16ed84b
Hari Govind <hgs@disroot.org>
2018-07-10 16:11:54 +0530

    Made sample configuration path absolute

--- config/copyConfig.js
+++ config/copyConfig.js
@@ -1,9 +1,10 @@
  // Script to copy config.sample.js to config.js
 
 const fs = require('fs')
+const path = require('path')
 
 try{
-var sample_config_file = fs.readFileSync('config/config.sample.js', 'utf8');
+var sample_config_file = fs.readFileSync(path.join(__dirname, 'config.sample.js'), 'utf8');
 } catch (err) {
     if (err.code === 'ENOENT') {
         console.log('Sample configuration file not found!');
@@ -14,7 +15,7 @@ var sample_config_file = fs.readFileSync('config/config.sample.js', 'utf8');
       
 }
 try{
-fs.writeFileSync('config/config.js',sample_config_file,'utf8');
+fs.writeFileSync(path.join(__dirname, 'config.js'),sample_config_file,'utf8');
 } catch (err) {
     console.log('Cannot write sample configuratio file.')
     throw err;
