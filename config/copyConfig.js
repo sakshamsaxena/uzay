00193d2cd352ba30b965660bde9cdf2aba645392
Hari Govind <hgs@disroot.org>
2018-07-10 20:34:58 +0530

    Prototype chaining

--- config/copyConfig.js
+++ config/copyConfig.js
@@ -9,15 +9,12 @@ const writeFile = util.promisify(fs.writeFile);
 
 readFile(path.join(__dirname, 'config.sample.js'))
     .then((content) => {
-       writeFile(path.join(__dirname, 'config.js'), content)
-            .then(() => {
-                console.log('Sample configuration sucessfully copied.');
-            })
-            .catch((err) => {
-                console.log('Error writing sample configuration file.', err)
-            });
+       return writeFile(path.join(__dirname, 'config.js'), content)
+    })
+    .then(() => {
+        console.log('Sample configuration sucessfully copied.');
     })
     .catch((err) => {
-        console.log('Error reading sample configuration file.', err);
+        console.log('Error copying sample configuration file.', err);
     });
 
